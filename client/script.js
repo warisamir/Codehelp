758a5f6cb3f240040f3f395b24cd3c72213813f8
WarisAmir <warisamir1918@yahoo.com>
2023-01-20 15:21:17 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codehelp-x4d3.onrender.com', {
+    const response = await fetch('https://codehelp-x4d3.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -102,7 +102,7 @@ const handleSubmit = async (e) => {
     if (response.ok) {
         const data = await response.json();
         const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
-        // console.log(parsedData)
+
         typeText(messageDiv, parsedData)
     } else {
         const err = await response.text()
